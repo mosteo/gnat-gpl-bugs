c3880ad22a3741543a4737720c3bbb4668f17931
Alejandro R. Mosteo <alejandro@mosteo.com>
2016-12-23 17:37:08 +0100

    Memory leak in holder container

---
+++ src/holder_leak.adb
@@ -0,0 +1,33 @@
+with Ada.Containers.Indefinite_Holders;
+with Ada.Text_IO; use Ada.Text_IO;
+
+--  To be run under valgrind
+--  Symptom:
+--  ==21853== LEAK SUMMARY:
+--  ==21853==    definitely lost: 15,984 bytes in 666 blocks
+--  ==21853==    indirectly lost: 18,648 bytes in 666 blocks
+
+procedure Holder_Leak is
+   use Ada.Containers;
+   type Indef is array (Integer range <>) of Integer;
+   package Holders is new Indefinite_Holders (Indef);
+   type Def is new Holders.Holder with null record;
+
+   -- This wrapper type is necessary for the leak to manifest
+   type Outer is record
+      Inner : Def;
+   end record;
+
+   procedure Eat (O : Outer) is
+   begin
+      for I of O.Inner.Constant_Reference loop
+      -- Using .Element instead there is no leak
+         Put_Line (I'Img);
+      end loop;
+   end Eat;
+
+begin
+   for I in 1 .. 666 loop
+      Eat ((Inner => To_Holder ((1, 2, 3, 4, 5))));
+   end loop;
+end Holder_Leak;
